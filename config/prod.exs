6d9b44c0654e9645744de9e60cc51371c9a251cc
Tran Anh Vu <eyelight93@gmail.com>
2019-09-26 12:11:34 +0700

    temp

--- config/prod.exs
+++ config/prod.exs
@@ -24,7 +24,7 @@ config :tilex, TilexWeb.Endpoint,
   url: [host: System.get_env("HOST"), port: 80, scheme: "https"],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  static_url: System.get_env("HOST")
+  static_url: [host: System.get_env("HOST")]
 
 # Do not print debug messages in production
 config :logger, level: :info
